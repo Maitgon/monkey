a3b8c0d277d6001f6e12f40437cb0aebb238c58c
Maitgon <aitor98gfgb@gmail.com>
2022-01-04 00:47:20 +0100

    adding parser

---
+++ ast/ast.go
@@ -0,0 +1,49 @@
+// ast/ast.go
+
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // The token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifiers are expressions
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (id *Identifier) expressionNode()      {}
+func (id *Identifier) TokenLiteral() string { return id.Token.Literal }
